44fe74925bcb8e91ded1163cc6ea5cd33c3a5c38
seymenjx <seymenozcelk@gmail.com>
2024-07-17 23:52:26 +0300

    basics

--- add-docs-pc.py
+++ add-docs-pc.py
@@ -5,6 +5,8 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_ai21 import AI21SemanticTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain.docstore.document import Document
+
+
 import os
 from dotenv import load_dotenv
 
