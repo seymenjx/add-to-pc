9f0ad37e050515cbaceca768ac4d7693d41ba3dd
seymenjx <seymenozcelk@gmail.com>
2024-07-18 00:01:53 +0300

    basics

--- add-docs-pc.py
+++ add-docs-pc.py
@@ -9,7 +9,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-
+#back to basics
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
